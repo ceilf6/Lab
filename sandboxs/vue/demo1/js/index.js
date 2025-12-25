64bb9d6886e1f81e2cdde29516b0fd0cc943f12e
WJH <3506456886@qq.com>
2025-12-25 22:32:57 +0800

    feat: 计算属性 - 防止冗余数据 & 计算函数, 通过 this 拿到界面数据

--- sandboxs/vue/demo1/js/index.js
+++ sandboxs/vue/demo1/js/index.js
@@ -6,11 +6,12 @@ var vm = new Vue({
     title: '淘东手机', // -> {{title}} 来使用
     goods: goods,
   },
-  // 计算属性
+  // 计算属性 - 防止冗余数据，由界面数据拓展得到的数据
   computed: {
+    // 计算函数
     count: function () {
       var sum = 0;
-      for (var i = 0; i < this.goods.length; i++) {
+      for (var i = 0; i < this.goods.length; i++) { // 通过 this 拿到界面数据
         sum += this.goods[i].choose;
       }
       return sum;
