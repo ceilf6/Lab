e211c127238988b10674d8ae514d2ef801b532fb
WJH <3506456886@qq.com>
2025-12-27 15:49:24 +0800

    feat: Fiber-defer

---
+++ JS/remoteAdd.js
@@ -0,0 +1,19 @@
+const remoteAdd = () => {
+
+
+    return Promise.
+}
+
+const target = [];
+
+const add = (...inputs) => {
+    let ans = 0;
+    const L = inputs.length;
+    if (L === 1) return inputs[0]
+    else if (L === 2) {
+
+    } else {
+
+    }
+    return ans;
+}
