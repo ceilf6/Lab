c42675a1850dfea928ae06760746f4e368bf3ef1
WJH <3506456886@qq.com>
2025-12-27 16:49:01 +0800

    feat: 二分比任务队列快

--- JS/remoteAdd.js
+++ JS/remoteAdd.js
@@ -1,28 +1,81 @@
-const remoteAdd = (a, b) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(a + b);
-        }, 1000)
+const remoteAdd = (a, b) =>
+    new Promise(resolve => {
+        setTimeout(() => resolve(a + b), 100);
     });
+
+async function add1(...inputs) {
+    const L = inputs.length;
+
+    if (L === 0) return 0;
+    if (L === 1) return inputs[0];
+
+    const MID = Math.floor(L / 2);
+
+    const [left, right] = await Promise.all([
+        add1(...inputs.slice(0, MID)),
+        add1(...inputs.slice(MID))
+    ]);
+
+    return remoteAdd(left, right); // 不应该用ans+=，因为这里需要用异步的服务
 }
 
 const target = [1, 2, 3, 4, 5, 6];
 
-async function add(...inputs) {
-    let ans = 0;
-    const L = inputs.length;
-    if (L === 1) return inputs[0]
-    else if (L === 2) {
-        const res = await remoteAdd(inputs[0], inputs[1]);
-        return res
-    } else {
-        const MID = Math.floor(L / 2);
-        const a1 = add(inputs.slice(0, MID));
-        const a2 = add(inputs.slice(MID + 1,));
-        ans += a1 + a2
-    }
-    return ans;
-}
+const add1start = Date.now();
+console.log('=== add1二分开始 ===')
+add1(...target).then(res => {
+    const add1end = Date.now();
+    console.log(`=== add1结束，答案 ${res} ，用时 ${add1end - add1start} ms ===`)
+})
+// 别忘记展开，因为形参就是 ...一个数组
 
-const res = add(...target); // 别忘记展开，因为形参就是 ...一个数组
-console.log(res);
+async function add2(...inputs) {
+    const tasks = [...inputs];
+
+    while (tasks.length > 1) {
+        const a = tasks.shift();
+        const b = tasks.shift();
+
+        const res = await remoteAdd(a, b);
+        tasks.push(res);
+    }
+
+    return tasks[0];
+}
+const add2start = Date.now();
+console.log('=== add2开始 ===')
+add2(...target).then(res => {
+    const add2end = Date.now();
+    console.log(`=== add2结束，答案 ${res} ，用时 ${add2end - add2start} ms ===`)
+})
+
+async function add3(nums) {
+    let tasks = [...nums];
+
+    while (tasks.length > 1) {
+        const nextRound = [];
+        const promises = [];
+
+        for (let i = 0; i < tasks.length; i += 2) {
+            if (i + 1 < tasks.length) {
+                promises.push(
+                    remoteAdd(tasks[i], tasks[i + 1])
+                );
+            } else {
+                // 奇数个，直接进入下一轮
+                nextRound.push(tasks[i]);
+            }
+        }
+
+        const results = await Promise.all(promises);
+        tasks = nextRound.concat(results);
+    }
+
+    return tasks[0];
+}
+const add3start = Date.now();
+console.log('=== add3开始 ===')
+add2(...target).then(res => {
+    const add3end = Date.now();
+    console.log(`=== add3结束，答案 ${res} ，用时 ${add3end - add3start} ms ===`)
+})
