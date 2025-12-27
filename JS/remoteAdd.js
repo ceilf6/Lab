6eb7da0f77b62f137ce7e85a84c7d6851b35fa8f
WJH <3506456886@qq.com>
2025-12-27 16:32:51 +0800

    feat: initial remoteAdd

--- JS/remoteAdd.js
+++ JS/remoteAdd.js
@@ -1,19 +1,28 @@
-const remoteAdd = () => {
-
-
-    return Promise.
+const remoteAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b);
+        }, 1000)
+    });
 }
 
-const target = [];
+const target = [1, 2, 3, 4, 5, 6];
 
-const add = (...inputs) => {
+async function add(...inputs) {
     let ans = 0;
     const L = inputs.length;
     if (L === 1) return inputs[0]
     else if (L === 2) {
-
+        const res = await remoteAdd(inputs[0], inputs[1]);
+        return res
     } else {
-
+        const MID = Math.floor(L / 2);
+        const a1 = add(inputs.slice(0, MID));
+        const a2 = add(inputs.slice(MID + 1,));
+        ans += a1 + a2
     }
     return ans;
 }
+
+const res = add(target);
+console.log(res);
