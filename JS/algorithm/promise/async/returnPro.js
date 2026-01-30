365a3ac547130543c9405211bf9d529877aa5430
WJH <3506456886@qq.com>
2026-01-30 22:28:38 +0800

    feat: async, await

--- JS/algorithm/promise/async/returnPro.js
+++ JS/algorithm/promise/async/returnPro.js
@@ -1,3 +1,6 @@
+// async 返回的一定是 Promise
+// 下面三种表示是等价的
+
 async function apro1() {
     return 'apro1';
 }
@@ -14,6 +17,15 @@ async function apro3(params) {
     return Promise.resolve('apro3')
 }
 
-setTimeout(() => console.log(apro1, apro1()), 2000)
-setTimeout(() => console.log(apro2, apro2()), 2000)
-setTimeout(() => console.log(apro3, apro3()), 2000)
+setTimeout(() => console.log(apro1, apro1()), 2000);
+setTimeout(() => console.log(apro2, apro2()), 2000);
+setTimeout(() => console.log(apro3, apro3()), 2000);
+
+// await 一定是 Promise
+// 如果不是的话会隐式转换
+(async () => {
+    const data1 = await 1;
+    const data2 = await Promise.resolve(2);
+    console.log(data1);
+    console.log(data2);
+})()
