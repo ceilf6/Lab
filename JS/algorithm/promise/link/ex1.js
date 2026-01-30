7838758d0dd2ca7187bb54f912b8fc8479bf43d6
WJH <3506456886@qq.com>
2026-01-30 20:12:37 +0800

    feat: promise

--- JS/algorithm/promise/link/ex1.js
+++ JS/algorithm/promise/link/ex1.js
@@ -1,22 +1,20 @@
 const pro1 = new Promise((resolve, reject) => {
-    // Promise 首次状态改变只能通过 resolve / reject 单向不可逆
+    // Promise 传入函数的参数为状态改变函数 resolve / reject
     resolve(1);
     reject(1);
 })
 
 const pro2 = pro1.then(res => {
-    // 在链中通过 return / throw
-    return res + 1;
-})
-
-const pro3 = pro2.then(res => {
+    // 在链中没有状态改变函数，得通过 return / throw
     throw res + 1;
 })
 
-const pro4 = pro3.catch(e => {
+const pro3 = pro2.catch(e => {
     return e + 1;
 })
 
+// Promise { 1 } Promise { <rejected> 2 } Promise { 3 }
+
 setTimeout(() => {
-    console.log(pro1, pro2, pro3, pro4)
+    console.log(pro1, pro2, pro3)
 }, 1000)
