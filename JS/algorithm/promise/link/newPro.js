bacdfb7856c42ca9c66c6f8944f19b0bba675fe7
WJH <3506456886@qq.com>
2026-01-30 18:37:43 +0800

    feat: 由于 pro2 返回的新任务没有结束     那么 pro2 和 pro3 状态都是     Promise { <pending> }

--- JS/algorithm/promise/link/newPro.js
+++ JS/algorithm/promise/link/newPro.js
@@ -2,10 +2,19 @@ const pro1 = new Promise((resolve, reject) => {
     resolve();
 });
 
+let pro3;
+
 const pro2 = pro1.then(() => {
-    return new Promise((resolve, reject) => { });
+    pro3 = new Promise((resolve, reject) => { });
+    return pro3;
 });
 
 setTimeout(() => {
     console.log(pro2);
+    /*
+    由于 pro2 返回的新任务没有结束
+    那么 pro2 和 pro3 状态都是
+    Promise { <pending> }
+    */
+    console.log(pro3);
 }, 1000);
