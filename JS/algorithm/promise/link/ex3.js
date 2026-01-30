cb9f905290678fbbacf2b73fc19577c822e03681
WJH <3506456886@qq.com>
2026-01-30 20:46:47 +0800

    feat: promise

--- JS/algorithm/promise/link/ex3.js
+++ JS/algorithm/promise/link/ex3.js
@@ -9,6 +9,30 @@ new Promise((resolve, reject) => {
         throw err; // 又抛错误 rejected Error(1)
         return 3;
     })
+    .then((res) => { // 不是后续->跟随
+        console.log(res);
+    }).catch(err => {
+        console.log('最终捕获:', err);
+    }); // 需要兜底 catch，否则就崩了看不了 P2 结果
+
+// 上面都是 rejected Error(1)
+
+
+const P2 = new Promise((resolve, reject) => {
+    resolve(1); // fulfilled 1
+})
     .then((res) => {
         console.log(res);
-    });
+        return new Error('2'); // fulfilled Error(2)
+    })
+    .catch((err) => { // 不是后续->跟随 fulfilled Error(2)
+        throw err;
+        return 3;
+    })
+    .then((res) => {
+        console.log(res); // fulfilled undefined
+    }).catch(err => {
+        console.log('最终捕获:', err);
+    }); // 需要兜底 catch，否则就崩了看不了 P2 结果
+
+setTimeout(() => console.log(P2), 1000)
