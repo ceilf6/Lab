afc0bd28afea3f93a40d4dd59c139469771c7d95
WJH <3506456886@qq.com>
2026-02-01 14:04:51 +0800

    feat: .cjs 后缀会永远用 CJS 会覆盖 package.json 中的 type: "module"

--- NodeJS/CMJ/log.cjs
+++ NodeJS/CMJ/log.cjs
@@ -1,5 +1,5 @@
 console.log(global.a);
 // undefined 全局对象不会被污染
 
-const { a, funcA } = require('./a') // 返回对象 { a, funcA }
+const { a, funcA } = require('./a.cjs') // 返回对象 { a, funcA }
 console.log(a)
