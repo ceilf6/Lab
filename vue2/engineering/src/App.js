153b85e9288b0ad5f219b317043cb4ca9c317b0c
WJH <3506456886@qq.com>
2026-02-01 13:34:12 +0800

    fix: module

--- vue2/engineering/src/App.js
+++ vue2/engineering/src/App.js
@@ -1,10 +1,12 @@
 // Vue 的根组件
-import CntButton from './components/cnt-button'
+import CntButton from './components/cnt-button.js'
 
 const template = `
-<h1> I\'m ceilf6 </h1>
-<CntButton />
+<div>
+  <h1> I\'m ceilf6 </h1>
+  <cnt-button></cnt-button>
 `
+// 在 Vue2 的 in‑DOM 模板里需要用 kebab-case，而且不要自闭合
 
 // 组件的本质就是一个配置对象
 export default {
