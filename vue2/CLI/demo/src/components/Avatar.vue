1936c633c99990ed17059e63e66d77ff0c8b357e
WJH <3506456886@qq.com>
2026-02-02 14:48:27 +0800

    feat: scoped => css module

--- vue2/CLI/demo/src/components/Avatar.vue
+++ vue2/CLI/demo/src/components/Avatar.vue
@@ -1,11 +1,20 @@
 <template>
-  <img :src="url" alt="" />
+  <img
+    :src="url"
+    alt=""
+    :style="{ width: width + 'px', height: height + 'px' }"
+  />
 </template>
 
 <script>
 export default {
   props: {
-    url: String,
+    url: {
+      type: String, // 类型必须是字符串
+      required: true, // 必传属性
+    },
+    width: Number,
+    height: Number,
   },
 };
 </script>
