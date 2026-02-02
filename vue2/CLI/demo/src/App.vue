d06aada9503dd385e669d64c82d934038bace136
WJH <3506456886@qq.com>
2026-02-02 14:48:27 +0800

    feat: scoped => css module

--- vue2/CLI/demo/src/App.vue
+++ vue2/CLI/demo/src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div id="app">
-    <Avatar :url="require('@/assets/ceilf6.png')" />
+    <Avatar
+      class="avatar-img"
+      :url="require('@/assets/ceilf6.png')"
+      :width="66"
+      :height="66"
+    />
+    <!-- 传递数字得通过 绑定bind: JS注入 -->
   </div>
 </template>
 
 <script>
 // SFC 高聚合
 
-import Avatar from './components/Avatar.vue';
+import Avatar from "./components/Avatar.vue";
 export default {
   components: {
-    Avatar
+    Avatar,
   },
 };
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
+// 参照 webpack 的 CSS module 的思想，为解决类样式冲突的问题，通过 scoped 就会开启样式的作用域
+// 到时候 类样式后面还会加上一个 [ ] 属性选择器确保选择的精确性
+.avatar-img {
+  border-radius: 50%;
+  display: block;
+}
 </style>
