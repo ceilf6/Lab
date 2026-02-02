97c0fd21c48daca3cad795d7863be6dbc79f05fe
WJH <3506456886@qq.com>
2026-02-02 21:05:22 +0800

    feat: init pager

---
+++ vue2/pager/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <pager></pager>
+  </div>
+</template>
+
+<script>
+import pager from "./components/pager.vue";
+export default {
+  components: {
+    pager,
+  },
+};
+</script>
+
+<style></style>
---
+++ vue2/pager/src/components/pager.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- 组件根元素类名一般有 组件名-container 
+  方便外层父组件预判内层根元素类名 => 作用域样式会对子组件根元素产生影响 -->
+  <div class="pager-container">
+    <a href="">&lt;&lt;</a>
+    <a href="">&lt;&lt;</a>
+
+    <a href="">1</a>
+    <a href="">2</a>
+    <a href="">3</a>
+    <a href="">4</a>
+    <a href="">5</a>
+    <a href="">6</a>
+    <a href="">7</a>
+    <a href="">8</a>
+    <a href="">9</a>
+    <a href="">10</a>
+
+    <a href="">&gt;&gt;</a>
+    <a href="">&gt;&gt;</a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
