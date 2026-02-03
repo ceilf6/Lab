5046144da5fb7c7d19913617decf498c3fb53fe8
WJH <3506456886@qq.com>
2026-02-03 11:23:08 +0800

    feat: 切换显示

--- vue2/pager/src/App.vue
+++ vue2/pager/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <pager :current="2"></pager>
+    <p v-if="visible">hello, I'm ceilf6'</p>
+    <button @click="changeVisible">切换显示</button>
   </div>
 </template>
 
@@ -9,6 +11,16 @@ import pager from "./components/pager.vue";
 export default {
   components: {
     pager,
+  },
+  data() {
+    return {
+      visible: true,
+    };
+  },
+  methods: {
+    changeVisible() {
+      this.visible = !this.visible;
+    },
   },
 };
 </script>
