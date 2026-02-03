83473c373c78ffee4a500a0c27eae55e35b38c69
WJH <3506456886@qq.com>
2026-02-03 22:43:42 +0800

    feat: 通过原型链判断是否为目标类型

--- JS/function/判断是否为数组.js
+++ JS/function/判断是否为数组.js
@@ -1,6 +1,12 @@
 // 1. 使用静态方法 Array.isArray()
 
-// 2. 用原型API 判断目标是否在数组的原型链上
+// 2. 用 原型API 判断目标是否在数组的原型链上
+// (1) isPrototypeOf
 const target = []; // document.get / query
-const ans = Array.prototype.isPrototypeOf(target)
-console.log(ans)
+const bool1 = Array.prototype.isPrototypeOf(target)
+console.log(bool1)
+
+// (2) instanceof
+const bool2 = target instanceof Array
+console.log(bool2)
+// 注意如果有iframe跨越了窗口Array会指向不一样
