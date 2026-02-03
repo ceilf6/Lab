16558cd444a7dd0e15164fe3f45d67c9fa52d0ef
WJH <3506456886@qq.com>
2026-02-03 22:03:23 +0800

    chore: toString => type

--- JS/function/toString.js
+++ JS/function/toString.js
@@ -2,8 +2,8 @@ const obj = new Object();
 const str = new String();
 
 console.log(obj.toString())
-console.log(Object.prototype.toString.call(obj))
-console.log(Object.prototype.toString.call(str))
+console.log(Object.prototype.toString.call(obj)) // [object Object]
+console.log(Object.prototype.toString.call(str)) // [object String]
 
 /*
 JS 对象有内部槽 [[Class]]
