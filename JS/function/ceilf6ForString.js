7a63cf3468ff9667d461dbb3df52c8e72d246d53
WJH <3506456886@qq.com>
2026-02-03 21:38:04 +0800

    feat: 字符串原型函数ceilf6

---
+++ JS/function/ceilf6ForString.js
@@ -0,0 +1,10 @@
+String.prototype.ceilf6 = function () {
+    console.log(this)
+    const newStr = new String('ceilf6')
+    return newStr
+}
+
+const str = new String('ceilf8')
+console.log(str)
+const newStr = str.ceilf6();
+console.log(newStr)
