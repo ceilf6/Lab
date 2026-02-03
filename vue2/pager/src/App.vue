2b4c432709eec4c0ccbded4779a2157e82400b82
WJH <3506456886@qq.com>
2026-02-03 13:21:51 +0800

    feat: 注册事件

--- vue2/pager/src/App.vue
+++ vue2/pager/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <pager :current="2" :total="302"></pager>
+    <pager
+      :current="current"
+      :total="total"
+      @pageChange="handlePageChange($event, newPage)"
+    ></pager>
 
     <p v-if="visible">v-if -> 没有vnnode -> 渲染节点数少</p>
     <p v-show="visible">v-show -> 始终有vnode=>DOM - 稳定</p>
@@ -17,12 +21,19 @@ export default {
   data() {
     return {
       visible: true,
+      total: 202,
+      current: 1,
     };
   },
   methods: {
     changeVisible() {
       this.visible = !this.visible;
     },
+  },
+  handlePageChange($event, newPage) {
+    // 注册事件 - 处理页码改变
+    this.current = newPage;
+    console.log(`加载第${this.current}页数据`);
   },
 };
 </script>
