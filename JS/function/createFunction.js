67c084bdd095b3319c4567a2412387403e40f272
WJH <3506456886@qq.com>
2026-02-03 18:59:12 +0800

    feat: new 声明

--- JS/function/createFunction.js
+++ JS/function/createFunction.js
@@ -21,3 +21,23 @@ const o4 = createObj();
 o3.sayHello();
 o4.sayHello();
 console.log('o3 === o4:', o3 === o4)
+
+
+// 构造函数一般是用大驼峰命名
+function CreateObj(name = 'ceilf5', age = 5) { // 注意是在形参位置设置默认值
+    // 构造函数：用于创建对象
+    // 即返回对象
+    // return {
+    //     name: name,
+    //     age: age,
+    //     // 可以简写为 age
+    //     sayHello() {
+    //         console.log(`I'm ${name}, ${age} years old`)
+    //     }
+    // }
+}
+// 使用构造函数得用 new 声明，这样就必定返回对象
+const newO = new CreateObj()
+const o = CreateObj();
+console.log('newO', newO)
+console.log('o', o)
