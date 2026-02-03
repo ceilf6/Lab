9c426f1e74e9af58c76370a01d8c588bfbaeaa20
WJH <3506456886@qq.com>
2026-02-03 12:59:29 +0800

    feat: 页码逻辑

--- vue2/pager/src/App.vue
+++ vue2/pager/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <pager :current="8" :total="102"></pager>
+    <pager :current="1" :total="302"></pager>
 
     <p v-if="visible">v-if -> 没有vnnode -> 渲染节点数少</p>
     <p v-show="visible">v-show -> 始终有vnode=>DOM - 稳定</p>
