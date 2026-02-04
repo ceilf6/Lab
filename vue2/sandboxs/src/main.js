e1095e548c1695f430198cc261c75a31c1a0c369
WJH <3506456886@qq.com>
2026-02-04 14:58:37 +0800

    feat: 抽离router，简化main.js

--- vue2/sandboxs/src/main.js
+++ vue2/sandboxs/src/main.js
@@ -2,24 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import './styles/global.less'
-import VueRouter from 'vue-router';
 
-import Home from './views/Home'
-import About from './views/About'
-import Blog from './views/Blog'
-import Message from './views/Message'
-
-Vue.use(VueRouter); // 使用Vue插件 - Router
-// 本质是构造函数
-const router = new VueRouter({
-  // 插件配置
-  routes: [
-    { path: "/", component: Home }, // 一个对象就是一条匹配规则
-    { path: "/about", component: About },
-    { path: "/blog", component: Blog },
-    { path: "/message", component: Message },
-  ]
-})
+import router from './router'
 
 new Vue({
   router: router,
