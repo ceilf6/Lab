61d582b17f320f3408c4d7027de29ae1b59ee40d
WJH <3506456886@qq.com>
2026-02-04 19:42:57 +0800

    feat: toast提示方法

---
+++ vue2/sandboxs/test/toast/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="showToast">点我提示</button>
+</template>
+
+<script>
+import toast from "@/utils/toast";
+
+window.toast = toast; // 挂载、方便测试
+
+export default {
+  methods: {
+    showToast() {
+      toast("操作成功", "success");
+    },
+  },
+};
+</script>
