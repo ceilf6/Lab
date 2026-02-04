a6f163295f89a57669e4a9b53e9c87b77ed88a71
WJH <3506456886@qq.com>
2026-02-04 14:44:59 +0800

    feat: 添加路由插件

--- vue2/sandboxs/src/main.js
+++ vue2/sandboxs/src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import './styles/global.less'
+import VueRouter from 'vue-router';
 
-// Vue.config.productionTip = false
+Vue.use(VueRouter); // 使用Vue插件 - Router
+// 本质是构造函数
+const router = new VueRouter()
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
