4e51a3c102fda68b777d78319bbf7897f18fca81
WJH <3506456886@qq.com>
2026-02-04 11:55:26 +0800

    chore: cli-service-global 4 test vue serve ./src/components/Pager/test.vue

---
+++ vue2/pager/src/components/Pager/test.vue
@@ -0,0 +1,41 @@
+<!-- vue serve ./src/components/Pager/test.vue -->
+
+<template>
+  <div id="app">
+    <pager
+      :current="current"
+      :total="total"
+      @pageChange="handlePageChange"
+    ></pager>
+    <!-- Vue 会自动将 $emit 的第二个参数传给处理函数的第一个参数 -->
+    <!-- @pageChange="(newPage) => handlePageChange(newPage)" -->
+  </div>
+</template>
+
+<script>
+import pager from "./";
+export default {
+  components: {
+    pager,
+  },
+  data() {
+    return {
+      visible: true,
+      total: 202,
+      current: 1,
+    };
+  },
+  methods: {
+    changeVisible() {
+      this.visible = !this.visible;
+    },
+    handlePageChange(newPage) {
+      // 注册事件 - 处理页码改变
+      this.current = newPage;
+      console.log(`加载第${this.current}页数据`);
+    },
+  },
+};
+</script>
+
+<style></style>
