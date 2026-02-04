ae1d437c3280fadc3adaccaadde445e4635ec566
WJH <3506456886@qq.com>
2026-02-04 12:07:10 +0800

    feat: Modal component

---
+++ vue2/sandboxs/src/components/Modal/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="message-container">
+    <button>确认</button>
+    <button>关闭</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.message-container {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border: 2px solid #bf91f3;
+  padding: 20px;
+}
+</style>
---
+++ vue2/sandboxs/src/components/Modal/test.vue
@@ -0,0 +1,11 @@
+<template>
+  <Modal />
+</template>
+
+<script>
+import Modal from "./";
+
+export default {
+  components: { Modal },
+};
+</script>
