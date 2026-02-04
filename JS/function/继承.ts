3d996a1b6d0a85a550832b706c70f87391498d3b
WJH <3506456886@qq.com>
2026-02-04 09:37:17 +0800

    feat: 继承

---
+++ JS/function/继承.ts
@@ -0,0 +1,48 @@
+// 参考资料 https://github.com/ceilf6/CPlusPlus/blob/main/.2025/template%26adaptor.md
+
+// TS 支持 protected 关键词
+class Base {
+    public name: string;
+    protected count: number;
+    private secret: string;
+
+    constructor(name: string) {
+        this.name = name;        // public
+        this.count = 0;          // protected
+        this.secret = 'shh';     // private
+    }
+
+    public sayHi() {
+        console.log(this.name);      // ✅
+        console.log(this.count);     // ✅
+        console.log(this.secret);    // ✅
+    }
+
+    protected inc() {
+        this.count++;
+    }
+
+    private reveal() {
+        console.log(this.secret);
+    }
+}
+
+class Child extends Base {
+    work() {
+        console.log(this.name);   // ✅ public
+        console.log(this.count);  // ✅ protected
+        // console.log(this.secret); // ❌ private
+
+        this.inc();               // ✅ protected
+        // this.reveal();          // ❌ private
+    }
+}
+
+const b = new Base('Alice');
+b.name;        // ✅ public
+// b.count;    // ❌ protected
+// b.secret;   // ❌ private
+
+const c = new Child('Bob');
+c.name;        // ✅ public
+// c.count;    // ❌ protected
