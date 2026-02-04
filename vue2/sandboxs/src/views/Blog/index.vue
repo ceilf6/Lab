3fdf3617f41b7a92405da0fc92a823d1ce5238fa
WJH <3506456886@qq.com>
2026-02-04 14:56:11 +0800

    feat: router-view

---
+++ vue2/sandboxs/src/views/Blog/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>welcome to ceilf6's Blogs</h1>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ vue2/sandboxs/src/views/Message/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Talk whatever wanna</h1>
+</template>
+
+<script>
+export default {};
+</script>
