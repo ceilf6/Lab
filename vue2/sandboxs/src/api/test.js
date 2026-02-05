399506b1e174afcff1b7724092e0dd050dbedfad
WJH <3506456886@qq.com>
2026-02-05 13:27:25 +0800

    feat: 通过开发服务器代理处理开发环境中的、浏览器同源策略导致的跨域问题

--- vue2/sandboxs/src/api/test.js
+++ vue2/sandboxs/src/api/test.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 async function getLab() {
-    const res = await axios.get("https://github.com/ceilf6/Lab/latest-commit/main")
+    const res = await axios.get("/ceilf6/Lab/branch-and-tag-count")
     console.log(res)
 }
 
@@ -11,4 +11,9 @@ getLab()
 跨域了
 （索引）:1 Access to XMLHttpRequest at 'https://github.com/ceilf6/Lab/latest-commit/main' from origin 'http://localhost:8080' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
 test.js:4  GET https://github.com/ceilf6/Lab/latest-commit/main net::ERR_FAILED 406 (Not Acceptable)
+*/
+
+/*
+成功了
+effect-picture/dev-cross-origin.png
 */
