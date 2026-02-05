517bf002ae407b0a2a221a4f7deb263e7f56e2ac
WJH <3506456886@qq.com>
2026-02-05 20:47:12 +0800

    feat: Loading

--- vue2/sandboxs/test/Lifecycle/FetchData.vue
+++ vue2/sandboxs/test/Lifecycle/FetchData.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
-    <h1 v-if="branches">branches: {{ branches }}</h1>
-    <h1 v-if="!branches">loading...</h1>
+    <h1 v-if="!isLoading">branches: {{ branches }}</h1>
+    <!-- <h1 v-if="!branches">loading...</h1> -->
+    <Loading v-if="isLoading" />
   </div>
 </template>
 
 <script>
-import getLab from "../../src/api/test";
+import getLab from "@/api/test";
+import { Loading } from "@/components";
 
 export default {
+  components: {
+    Loading,
+  },
   data() {
     return {
       branches: 0,
       offset: 10,
+      isLoading: true,
     };
   },
   async created() {
@@ -22,6 +28,8 @@ export default {
     this.branches = res.branches + this.offset; // 如果在 beforeCreated 阶段就取不到 this.
 
     // 同时 async 语法糖本质是 Promise 所以数据更新其实是异步回调，并不会等 created() 结束后再往下 beforeMount
+
+    this.isLoading = false; // 异步完成后结束loading
   },
   updated() {
     console.log("updated");
