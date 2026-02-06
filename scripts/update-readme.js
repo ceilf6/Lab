bfcfe3cb648423d5b42b902950820d90773c9832
WJH <3506456886@qq.com>
2026-02-06 11:10:30 +0800

    feat[ptr]: auto update Action for README package.json 设置了 type: module require => import

--- scripts/update-readme.js
+++ scripts/update-readme.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// 获取 __dirname 的 ES Module 等价物
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /**
  * 从 commit message 中提取内容
