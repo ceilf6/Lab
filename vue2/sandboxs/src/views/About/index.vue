f3cf40c241193ef76927b634c28002670f78d1dd
WJH <3506456886@qq.com>
2026-02-06 18:58:40 +0800

    chore: update ARCH

--- vue2/sandboxs/src/views/About/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <h1>About ceilf6</h1>
-</template>
-
-<script>
-export default {};
-</script>
--- vue2/sandboxs/test/Lifecycle/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Lifecycle } from "./Lifecycle.vue";
-export { default as FetchData } from "./FetchData.vue";
-export { default as mountedDOM } from "./moutedDOM.vue";
-export { default as timerDestroyed } from "./timerDestroyed";
