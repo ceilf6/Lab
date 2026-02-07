a377d664cbe4bfaeaae48cbe73e00e89c6646582
WJH <3506456886@qq.com>
2026-02-07 11:13:38 +0800

    feat[ptr]: React 自定义组件想要触发DOM事件，得传递到能响应的元素上 同时注意如果没有特殊处理，在事件处理函数中，this指向undefined 1. bind 2. () =>

--- React/sandboxs/src/test/Event.js
+++ React/sandboxs/src/test/Event.js
@@ -9,11 +9,36 @@ export function Btn() {
 }
 
 export class MyBtn extends React.Component {
+    // 1.1
+    // constructor(props) {
+    //     super(props)
+    //     this.handleClick = this.handleClick.bind(this)
+    // }
+
+    handleClick() {
+        // console.log(this) // undefined
+        this.props.callback()
+        this.props.onClick()
+    }
+
+    // 2.1 用箭头函数那么事件处理函数不在原型上，而是在对象上
+    handleClick2 = () => {
+        this.props.callback()
+        this.props.onClick()
+    }
+
     render() {
         return (
-            <button onClick={this.props.callback}>
+            // 1.2
+            // <button onClick={this.handleClick.bind(this)}>
+
+            // <button onClick={this.handleClick2}>
+
+            // 2.2
+            <button onClick={() => this.handleClick()}>
                 点我
-            </button>
+                {/* <h1 onClick={this.props.onClick}>点我</h1> */}
+            </button >
         )
     }
 }
