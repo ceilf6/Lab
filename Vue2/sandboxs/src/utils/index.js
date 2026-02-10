f08f4332ebc3a8a006e7792c45a6bf7b94fc6f17
WJH <3506456886@qq.com>
2026-02-10 11:18:57 +0800

    feat[ptr]: 通过 vuex 进行共享数据管理 mutations 方法中不允许副作用操作 通过在 actions 中 dispatch 实现副作用操作

--- Vue2/sandboxs/src/utils/index.js
+++ Vue2/sandboxs/src/utils/index.js
@@ -1,3 +1,4 @@
 export { default as toast } from './toast';
 export { default as getComponentRootDom } from './getComponentRootDom';
 export { default as formatDate } from './formatDate'
+export { default as delay } from './delay'
