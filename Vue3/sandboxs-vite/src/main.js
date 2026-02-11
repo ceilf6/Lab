e44a05276f8472f34bd48cfd936639faa10b0cd7
WJH <3506456886@qq.com>
2026-02-11 17:50:59 +0800

    feat[ptr]: 因为 Vite 利用的是“现代浏览器支持ESM”这一特性，在开发时将模块文件直接传输给浏览器，不需要打包，是实时编译 所以 CMJ 是不支持的

--- Vue3/sandboxs-vite/src/main.js
+++ Vue3/sandboxs-vite/src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+// require('./index.css') // 直接报错
+/*
+因为 Vite 利用的是“现代浏览器支持ESM”这一特性，在开发时将模块文件直接传输给浏览器，不需要打包，是实时编译
+所以 CMJ 是不支持的
+*/
 
 /*
 new Vue({
