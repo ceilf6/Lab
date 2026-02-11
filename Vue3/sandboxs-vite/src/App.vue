c768c72424c33d383d67c33783e300044006bcf4
WJH <3506456886@qq.com>
2026-02-11 22:15:49 +0800

    feat[ptr]: Vue3 双向绑定 v-model :modelValue , @update:modelValue v-model修饰符

--- Vue3/sandboxs-vite/src/App.vue
+++ Vue3/sandboxs-vite/src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <!-- <h1>=== 静态节点 ===</h1> -->
-  <Todos />
+  <!-- <Todos /> -->
   <!-- <PreStr></PreStr> -->
+
+  <!-- <CheckEditor :modelValue="checked" @update:modelValue="checked = $event" /> -->
+  <CheckEditor
+    v-model:checked="checkedVal"
+    v-model:title.trim="titleVal"
+  ></CheckEditor>
+  <h1>{{ checkedVal }}</h1>
+  <h1>{{ titleVal }}</h1>
 </template>
 
 <script>
-import Todos from "./components/Todos.vue";
+import { Todos, CheckEditor } from "./components";
 import PreStr from "./test/PreStr.vue";
 
 export default {
   components: {
     Todos,
     PreStr,
+    CheckEditor,
+    CheckEditor,
+  },
+  data() {
+    return {
+      checkedVal: true,
+      titleVal: "ceilf6",
+    };
   },
 };
 </script>
