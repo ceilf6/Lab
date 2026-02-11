8c19bd8253e13a32ab5667c09bb5f56680596dcf
WJH <3506456886@qq.com>
2026-02-11 22:51:30 +0800

    feat[ptr]: 当使用`v-if v-else-if v-else`分支的时候，不再需要像Vue2一样指定`key`值（否则可能数据没清空) vue3会自动给予每个分支一个唯一的key 即便要手工给予key值，也必须给予每个分支唯一的key，不能因为要重用分支而给予相同的key，否则就无法切换了（Block了、编译成静态节点了）

--- Vue3/sandboxs-vite/src/App.vue
+++ Vue3/sandboxs-vite/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <Checkbox />
+  <IfElse />
 </template>
 
 <script>
 import { Checkbox } from "./components";
+import IfElse from "./test/IfElse.vue";
 
 export default {
   components: {
     Checkbox,
+    IfElse,
   },
 };
 </script>
