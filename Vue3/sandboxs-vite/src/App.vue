bda6e7dc705f792f460fc5c1549b168749395b75
WJH <3506456886@qq.com>
2026-02-11 19:22:32 +0800

    feat[ptr]: Vue3 预字符串化

--- Vue3/sandboxs-vite/src/App.vue
+++ Vue3/sandboxs-vite/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <!-- <h1>=== 静态节点 ===</h1> -->
   <Todos />
+  <!-- <PreStr></PreStr> -->
 </template>
 
 <script>
 import Todos from "./components/Todos.vue";
+import PreStr from "./test/PreStr.vue";
 
 export default {
   components: {
     Todos,
+    PreStr,
   },
 };
 </script>
---
+++ Vue3/sandboxs-vite/src/test/PreStr.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="menu-bar-container">
+    <div class="logo">
+      <h1>logo</h1>
+    </div>
+    <ul class="nav">
+      <li><a href="">menu</a></li>
+      <li><a href="">menu</a></li>
+      <li><a href="">menu</a></li>
+      <li><a href="">menu</a></li>
+      <li><a href="">menu</a></li>
+    </ul>
+    <div class="user">
+      <span>{{ user.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "ceilf6",
+      },
+    };
+  },
+};
+</script>
