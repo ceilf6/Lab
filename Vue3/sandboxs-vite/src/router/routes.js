15e8c07587ad85e610fa6b9f20f0d51808e9407d
WJH <3506456886@qq.com>
2026-02-12 14:14:46 +0800

    feat[ptr]: 异步加载页面（本质就是异步加载组件）

--- Vue3/sandboxs-vite/src/router/routes.js
+++ Vue3/sandboxs-vite/src/router/routes.js
@@ -1,7 +1,10 @@
-import Home from '../views/Home/index.vue'
-import About from '../views/About/index.vue'
+// import Home from '../views/Home/index.vue'
+// import About from '../views/About/index.vue'
 // routes 规则 和 Vue2 的 router 类似
+
+import { syncGetComp } from '../utils'
+
 export default [
-    { path: "/", component: Home },
-    { path: '/about', component: About }
+    { path: "/", component: syncGetComp("../views/Home/index.vue") },
+    { path: '/about', component: syncGetComp("../views/About/index.vue") }
 ]
