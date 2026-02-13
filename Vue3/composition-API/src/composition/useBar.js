c4eb966644d4fffc905455ff32ff66717ab0d5ce
WJH <3506456886@qq.com>
2026-02-13 22:17:23 +0800

    chore: some adds

--- Vue3/composition-API/src/composition/useBar.js
+++ Vue3/composition-API/src/composition/useBar.js
@@ -1,7 +1,10 @@
 import { computed, ref, watch } from "vue";
 import gsap from "gsap";
 const colors = ["#334552", "#B34335", "#6E9FA5", "#A2C3AC", "#C8846C"];
-export default function useGdpBar(maxSize, gdp) {
+
+// 在 Bar1, Bar2 都能复用
+// Vue3 开发粒度从 Vue2组件级别 ⇒ 功能实现级别，方便复用
+export default function useGdpBar(maxSize, gdp) { // bars -> gdpRef -> props.gdp
   const max = computed(() => {
     if (gdp.value.length) {
       return Math.max(...gdp.value.map((it) => it.value));
@@ -31,6 +34,9 @@ export default function useGdpBar(maxSize, gdp) {
           };
         }
         gsap.to(bars.value[i], {
+          // 动画：从一个状态到另一个状态
+          // 变化数据 => JS动画
+          // bars.value[i] 中的属性 逐步变化到 barsTarget.value[i]
           ...newValue[i],
           duration: 1,
         });
