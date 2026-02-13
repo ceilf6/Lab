bf55d9fa12e3a07e9c7b76e62fb31be57496ed0c
WJH <3506456886@qq.com>
2026-02-13 21:18:50 +0800

    feat[ptr]: renderTriggered 在响应数据每变化一次就会触发一次 这两个生命周期钩子一般用于开发排查

---
+++ Vue3/sandboxs-vite/src/test/LifeCycle/RenderTriggered.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="count++">click</button>
+</template>
+
+<script>
+import { onRenderTriggered } from "vue";
+import { ref } from "vue";
+
+export default {
+  setup() {
+    let count = ref(0);
+
+    onRenderTriggered((e) => {
+      console.log(e);
+    });
+
+    return {
+      count,
+    };
+  },
+};
+</script>
