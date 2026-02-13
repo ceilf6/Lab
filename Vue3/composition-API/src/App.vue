f20c8516bf2cd09378965b17f826082949dbfe38
WJH <3506456886@qq.com>
2026-02-13 21:40:59 +0800

    chore: clean useless logo

--- Vue3/composition-API/src/App.vue
+++ Vue3/composition-API/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <h1>2019 GDP Top 5</h1>
+  <h1>2025 GDP</h1>
   <div class="container">
     <Bar1 :gdp="gdp" />
     <Bar2 :gdp="gdp" />
   </div>
   <div class="controls">
-    <div class="item" v-for="item in gdp">
+    <div class="item" v-for="item in gdp" :key="item.country">
       <label>{{ item.country }}</label>
       <input type="number" step="0.001" min="0" v-model="item.value" />
     </div>
@@ -60,4 +60,3 @@ h1 {
   text-align: center;
 }
 </style>
-
