cb3e408a5c268ce7a097211b014f9bd90b1e9207
WJH <3506456886@qq.com>
2026-02-13 22:17:23 +0800

    chore: some adds

--- Vue3/composition-API/vite.config.js
+++ Vue3/composition-API/vite.config.js
@@ -1,8 +1,10 @@
-export default {
-    proxy: {
-        "/api": {
-            target: "http://127.0.0.1:5500/Vue3/composition-API",
-            rewrite: (path) => path.replace(/^\/api/, ""), // 重写去除 /api
-        },
-    },
-};
+module.exports = { // 初代不需要 import { defineConfig } from 'vite' 
+    // export default defineConfig
+    proxy: { // 初代不需要将 proxy 包在 server 中
+        '/api': {
+            target: 'http://127.0.0.1:5500/Vue3/composition-API',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, '') // 重写去除 /api
+        }
+    }
+}
