24fee0107f5017f7721b2d6d833c2f8666e0ce8e
WJH <3506456886@qq.com>
2026-02-13 17:14:04 +0800

    feat[ptr]: Vue3生命周期 在 template ⇒ render 中渲染虚拟节点的时候会对响应式数据收集依赖 renderTracked 在收集到一个依赖就会触发一次

--- Vue3/sandboxs-vite/src/App.vue
+++ Vue3/sandboxs-vite/src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <Page></Page>
+  <!-- <Page></Page> -->
+  <RenderTracked />
 </template>
 
 <script>
-import Page from "./test/Page.vue";
+// import Page from "./test/Page.vue";
+import RenderTracked from "./test/RenderTracked.vue";
 export default {
   components: {
-    Page,
+    RenderTracked,
   },
 };
 </script>
---
+++ Vue3/sandboxs-vite/src/test/RenderTracked.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ num }}</h1>
+  <h1>{{ msg }}</h1>
+  <h1>{{ obj.name }}</h1>
+  <h1>{{ obj.age }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: 1,
+      msg: "nihao",
+      obj: {
+        name: "ceilf6",
+        age: 22,
+      },
+    };
+  },
+  renderTracked(e) {
+    console.log(e);
+  },
+};
+</script>
