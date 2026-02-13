38c942363d279014cf0c55e545fdd8ec6bdb8c2d
WJH <3506456886@qq.com>
2026-02-13 21:18:50 +0800

    feat[ptr]: renderTriggered 在响应数据每变化一次就会触发一次 这两个生命周期钩子一般用于开发排查

--- Vue3/sandboxs-vite/src/App.vue
+++ Vue3/sandboxs-vite/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <!-- <Page></Page> -->
-  <RenderTracked />
+  <RenderTriggered />
 </template>
 
 <script>
+import RenderTriggered from "./test/LifeCycle/RenderTriggered.vue";
+
 // import Page from "./test/Page.vue";
-import RenderTracked from "./test/LifeCycle/RenderTracked.vue";
+// import RenderTracked from "./test/LifeCycle/RenderTracked.vue";
 export default {
   components: {
-    RenderTracked,
+    RenderTriggered,
   },
 };
 </script>
