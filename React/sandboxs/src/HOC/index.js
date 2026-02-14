c63c2cf84120a4a30a77c61e5795386b58105fcc
WJH <3506456886@qq.com>
2026-02-14 19:57:00 +0800

    feat[ptr]: 装饰器模式重构 - 抽象横向关注点、抽离重复渲染事情给 HOC，不用重复书写渲染

--- React/sandboxs/src/HOC/index.js
+++ React/sandboxs/src/HOC/index.js
@@ -2,3 +2,5 @@ export { default as withLog } from './withLog'
 export { default as withLogin } from './withLogin'
 
 export { default as allApp } from './allApp'
+
+export { default as withDataGroup } from './withDataGroup'
