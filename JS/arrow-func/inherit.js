e4375fd586f419c268c9b1a66ccbc1eb6cb1b662
WJH <3506456886@qq.com>
2026-02-27 21:19:32 +0800

    chore: add

--- JS/arrow-func/inherit.js
+++ JS/arrow-func/inherit.js
@@ -7,6 +7,7 @@ function outer() {
     console.log('=== prototype', inner.prototype)
     inner('inner')
     // const innerIns = new inner() // error - TypeError: inner is not a constructor
+    // 由于箭头函数实例没有this和prototype ，所以自然就没有构造函数
 }
 
 new outer('arguments from outer')
