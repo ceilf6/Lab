f6bcd8cf4b714d76b595ebc172be1bb382cf223d
WJH <3506456886@qq.com>
2026-03-04 22:27:22 +0800

    chore: throttle

---
+++ JS/algorithm/throttle.js
@@ -0,0 +1,10 @@
+export function throttle(fn, wait) {
+    let lastTime = 0
+    return function (...args) {
+        const now = Date.now()
+        if (now - lastTime > wait) {
+            lastTime = now
+            fn.apply(this, args)
+        }
+    }
+}
