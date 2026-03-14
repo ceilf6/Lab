4366fbaea310de57dfccb5010412fe1a1849152b
WJH <3506456886@qq.com>
2026-03-14 13:02:22 +0800

    chore: 包装类test

---
+++ JS/algorithm/NumWithStr.js
@@ -0,0 +1,7 @@
+let a = '2026'
+let b = a
+let c = a
+
+b--
+c += 1
+console.log(a, b, c)
---
+++ JS/algorithm/Number.js
@@ -0,0 +1,9 @@
+console.log(!!Number(['0']))
+console.log(!!Number(['1']))
+
+console.log(Number(['0', '1']))
+console.log(!!Number(['0', '1']))
+
+console.log([].toString())
+console.log(Number(''))
+console.log(Number([]))
