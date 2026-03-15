accde32068d0aa8c35a80a6d8d167c11060ad7a9
WJH <3506456886@qq.com>
2026-03-15 21:28:23 +0800

    fix[ptr]: 缓存、超时...的请求包装函数中的一些细节问题

--- JS/algorithm/pDd/2026春/请求函数.js
+++ JS/algorithm/pDd/2026春/请求函数.js
@@ -1,14 +1,15 @@
 // 3. 要求实现一个有缓存、不会重复发送请求、超时报错的请求函数
 function createCachedRequest(requestFn, options = {}) {
-    const memo = new Map()
-    const ttl = options.ttl ? options.ttl : 5000
-    const timeout = options.timeout
-    const curTasks = new Map() // 保存异步任务Promise，防止多次请求    
+    const memo = new Map() // 闭包实现缓存
+    const curTasks = new Map() // 保存异步任务Promise，防止多次请求
 
-    return async (key) => {
+    const ttl = options.ttl ?? 5000
+    const timeout = options.timeout
+
+    return async function (key) {
         if (memo.has(key)) {
             const memoObj = memo.get(key)
-            const lastTime = memoObj.get(time)
+            const lastTime = memoObj.time // 注意是普通对象
             if (Date.now() - lastTime < ttl) {
                 return memoObj.val // 命中缓存
             }
@@ -23,9 +24,9 @@ function createCachedRequest(requestFn, options = {}) {
             return res
         }
         if (!timeout) {
-            curTasks.set(key, async () => {
-                return await requestFn(key)
-            })
+            // 如果存储的是 async () => {} 那就是一个返回Promise的函数，正确的应该是存储promise
+            const promise = requestFn(key)
+            curTasks.set(key, promise)
             const res = await curTasks.get(key) // 没设置timeout不可能超时
             memo.set(key, {
                 time: Date.now(),
@@ -34,7 +35,8 @@ function createCachedRequest(requestFn, options = {}) {
             return res
         }
         else {
-            curTasks.set(key, () => new Promise(
+            // 直接存储 new Promise 
+            curTasks.set(key, new Promise(
                 (resolve, reject) => {
                     // 利用 Promise 状态只可能改变一次，同时使用 reject 和 resolve
                     setTimeout(() => {
@@ -43,9 +45,13 @@ function createCachedRequest(requestFn, options = {}) {
                     const res = requestFn(key)
                     resolve(res)
                 }
-            ))
+            ).finally(() => { // 结束后清理任务（无论成功与否）
+                curTasks.delete(key)
+            }))
             const res = await curTasks.get(key)
-            if (res === 'Timeout') return 'Timeout'
+            if (res === 'Timeout')
+                throw new Error('Timeout') // 超时本质上是失败，应该报错
+            // return 'Timeout'
             memo.set(key, {
                 time: Date.now(),
                 val: res
