6941abc139d0cd1ecda56a8d3abba5fc182e8da5
WJH <3506456886@qq.com>
2026-03-15 21:02:01 +0800

    fix[ptr]: 新旧数组对比diff算法实现上的一些细节问题

--- JS/algorithm/pDd/2026春/数组比较.js
+++ JS/algorithm/pDd/2026春/数组比较.js
@@ -8,50 +8,61 @@ function diffList(oldList, newList, key) {
 
     const LO = oldList.length
     const LN = newList.length
-    oldList.sort((a, b) => a.id - b.id)
-    newList.sort((a, b) => a.id - b.id)
+    // sort 会影响原先数组,最好先拷一份再在新的上面处理
+    const oldArr = oldList.sort((a, b) => {
+        if (a[key] === b[key]) return 0
+        return a[key] > b[key] ? 1 : -1 // 兼顾字符串的情况
+    })
+    const newArr = newList.sort((a, b) => {
+        if (a[key] === b[key]) return 0
+        return a[key] > b[key] ? 1 : -1
+    })
     let oldPtr = 0
     let newPtr = 0
     while (oldPtr < LO && newPtr < LN) {
-        const old = oldList[oldPtr]
-        const new = newList[newPtr]
-        if (old.id === new.id) {
+        const oldItem = oldArr[oldPtr]
+        const newItem = newArr[newPtr]
+        if (oldItem.id === newItem.id) {
             const fields = []
             // 因为我看题目没有说新增字段，所以就直接取旧对象的属性进行比较
-            const keys = Object.keysOf(old)
+            const keys = Object.keys(oldItem)
+            const keys = [...new Set([...Object.keys(oldItem), ...Object.keys(newItem)])]
+            // 如果需要的话可以通过 ...new Set 取并集
             for (const key of keys) {
-                if (old[key] !== new [key]) {
+                if (oldItem[key] !== newItem[key]) {
                     fields.push({
                         field: key,
-                        oldVal: old[key],
-                        newVal: new [key]
+                        oldVal: oldItem[key],
+                        newVal: newItem[key]
                     })
                 }
             }
-            ans.changed.push({
-                id: old.id,
-                fields: fields
-            })
+            if (fields.length) { // 注意防空处理
+                ans.changed.push({
+                    id: oldItem.id,
+                    fields: fields
+                })
+            }
             oldPtr++
             newPtr++
         }
-        else if (old.id < new.id) {
-            ans.removed.push(old)
+        else if (oldItem.id < newItem.id) {
+            ans.removed.push(oldItem)
             oldPtr++
         }
-        else if (old.id > new.id) {
-            ans.added.push(new)
+        else if (oldItem.id > newItem.id) {
+            ans.added.push(newItem)
             newPtr++
         }
     }
 
     // 剩余情况处理
     while (oldPtr < LO) {
-        ans.removed.push(oldList[oldPtr])
+        ans.removed.push(oldArr[oldPtr])
         oldPtr++
     }
     while (newPtr < LN) {
-        ans.added.push(newList[newPtr])
+        ans.added.push(newArr[newPtr])
         newPtr++
     }
 
@@ -59,3 +70,18 @@ function diffList(oldList, newList, key) {
 }
 // 思路：双指针比较，利用排序后的id进行判断是哪种类型的变更
 // 并且题目说层数只需要一层，所以也不用写边界条件去递归DFS
+
+
+const oldList = [
+    { id: 1, name: 'ceilf6', price: 100 },
+    { id: 3, name: 'ceilf7', price: 100 },
+    { id: 5, name: 'ceilf8', price: 200 },
+    { id: 6, name: 'ceilf9', price: 200 }
+]
+const newList = [
+    { id: 2, name: 'ceilf6', price: 100 },
+    { id: 3, name: 'ceilf7', price: 100 },
+    { id: 4, name: 'ceilf8', price: 200 },
+    { id: 6, name: 'ceilf10', price: 200 }
+]
+console.log(diffList(oldList, newList, 'id'))
