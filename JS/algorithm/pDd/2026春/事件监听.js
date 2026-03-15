4030f33694e0b80b75967323bab4f840079baa45
WJH <3506456886@qq.com>
2026-03-15 20:40:27 +0800

    fix[ptr]: 事件总线实现上的一些细节问题

--- JS/algorithm/pDd/2026春/事件监听.js
+++ JS/algorithm/pDd/2026春/事件监听.js
@@ -6,7 +6,7 @@ function createEventBus() {
 // 只有_内部函数才能操作 listeners
 // 在内部用于添加回调函数的工具函数
 // 通过单调递增的 id 确保不会因为删除而有重复 id
-createEventBus.prototype._add = (event, handler, once) => {
+createEventBus.prototype._add = function (event, handler, once) {
     if (!this.listeners.has(event)) { // 初始化
         this.listeners.set(event, [0])
     }
@@ -14,17 +14,17 @@ createEventBus.prototype._add = (event, handler, once) => {
     const newId = pre[0] + 1
     const curObj = {
         id: newId,
-        handler: () => handler(),
-        flag: once // 是否只执行一次就移除
+        handler: handler,
+        once: once // 是否只执行一次就移除 // 注意变量名的统一
     }
-    const new = [...pre, curObj]
-    new [0] = newId
-    this.listeners.set(event, new)
+    const newVal = [...pre, curObj]
+    newVal[0] = newId
+    this.listeners.set(event, newVal)
     return newId // 返回 newId 用于删除
 }
 
 // 在内部用于删除回调函数的工具函数
-createEventBus.prototype._delete = (event, id) => {
+createEventBus.prototype._delete = function (event, id) {
     const cur = this.listeners.get(event)
     if (cur[0] < id) return false // 如果最大的 id 都比当前输入的小，那么肯定输错了
     let targetIdx = -1
@@ -34,9 +34,10 @@ createEventBus.prototype._delete = (event, id) => {
             break
         }
     }
+    // 或者用 .filter 筛除
     if (targetIdx === -1) return false
-    const new = cur.splice(targetIdx, 1)
-    this.listeners.set(event, new)
+    cur.splice(targetIdx, 1)
+    this.listeners.set(event, cur)
     return true // 删除成功
 }
 
@@ -54,8 +55,11 @@ createEventBus.prototype.once = function (event, handler) {
 
 createEventBus.prototype.emit = function (event, ...args) {
     const arr = this.listeners.get(event)
-    for (const item of arr) {
-        item.handler(args)
+    if (!arr) return // 防空处理
+    // for (const item of arr) {
+    for (let i = 1; i < arr.length; i++) { // 注意[0]存储的是ID
+        const item = arr[i]
+        item.handler(...args) // 注意传参
         if (item.once) {
             this._delete(event, item.id)
         }
@@ -65,3 +69,8 @@ createEventBus.prototype.emit = function (event, ...args) {
 // 只有_内部函数才能操作 listeners
 // 在内部用于添加回调函数的工具函数
 // 通过单调递增的 id 确保不会因为删除而有重复 id
+
+
+const bus = new createEventBus()
+bus.on('click', name => console.log(`你好 ${name}`))
+bus.emit('click', 'ceilf6')
