841011308c907864a76f5464a2f4b9f962f5b639
WJH <3506456886@qq.com>
2026-03-20 14:11:01 +0800

    feat: BigInt API test

--- JS/API/BigInt.js
+++ JS/API/BigInt.js
@@ -3,3 +3,12 @@ const ten2 = 10n
 
 console.log(ten1 == ten2) // true
 console.log(ten1 === ten2) // false
+
+// console.log(ten2 * 1) // TypeError: Cannot mix BigInt and other types, use explicit conversions
+console.log(ten2) // 10n
+console.log(Number(ten2)) // 10
+console.log(ten2 * 1n)
+console.log(ten2 * BigInt(1))
+
+// Math API 不支持 BigInt
+// console.log(Math.max(ten2, 100n)) // TypeError: Cannot convert a BigInt value to a number
