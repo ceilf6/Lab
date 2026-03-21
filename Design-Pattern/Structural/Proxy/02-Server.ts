f0d2ec1a0ccdafbdc44abc45052436a136e9fc62
WJH <3506456886@qq.com>
2026-03-21 15:27:54 +0800

    fix: Proxy - 需要 () 因为 === 的优先级比 位运算 优先级高

--- Design-Pattern/Structural/Proxy/02-Server.ts
+++ Design-Pattern/Structural/Proxy/02-Server.ts
@@ -1,7 +1,7 @@
-import { IService } from "./01-Interface";
+import { IServer } from "./01-Interface";
 
 // 提供真实服务的类
-export class Server implements IService {
+export class Server implements IServer {
     operation(): void {
         console.log("Service operation called");
     }
