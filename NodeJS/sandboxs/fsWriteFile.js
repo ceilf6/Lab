e456acc8f23ab07fec0f89631c8b993eaf281a7c
WJH <3506456886@qq.com>
2026-03-28 20:18:34 +0800

    feat[ptr]: 实现文件复制 read write < copyFile < stream pipe < stream drain-resume

--- NodeJS/sandboxs/fsWriteFile.js
+++ NodeJS/sandboxs/fsWriteFile.js
@@ -24,14 +24,56 @@ console.log('1. ', fs.readFileSync(absPath, "utf-8")); // 注意分号，和IIFE
     console.log('3. ', fs.readFileSync(absPath, "utf-8"))
 
     const bufferIns = Buffer.from("ceilf7", "utf-8")
-    await fs.promises.writeFile(path.resolve(absPath, "../2.txt"), bufferIns)
+    await fs.promises.writeFile(path.resolve(absPath, "../3.txt"), bufferIns)
     // 文件不存在会新建，但是目录不存在就报错了、因为毕竟只是writeFile
 
+    const from = path.resolve(__dirname, "./testFiles/from.txt")
+    const to = (i) => path.resolve(__dirname, `./testFiles/to${i}.txt`)
     // 实现复制
     // 1. copyFile
-    // 2. 先 read 再 write(二进制)
-    const fileContent = fs.readFileSync("/Users/a86198/Desktop/Lab/assetsForTest/cat.jpg")
-    await fs.promises.writeFile(path.resolve(absPath, "../copyPic.jpg"), fileContent)
+    console.time("1. copyFile")
+    await fs.promises.copyFile(from, to(1))
+    console.timeEnd("1. copyFile")
 
-    await fs.promises.unlink(absPath) // 删除文件
+    // 2. 先 read 再 write(二进制)
+    console.time("2. 先 read 再 write")
+    const fileContent = fs.readFileSync(from)
+    await fs.promises.writeFile(to(2), fileContent)
+    console.timeEnd("2. 先 read 再 write")
+
+    // 3. 文件流
+    console.time("3. 文件流")
+    const rs = fs.createReadStream(from)
+    const ws = fs.createWriteStream(to(3))
+    rs.on("data", chunk => {
+        // 读到一部分数据
+        const flag = ws.write(chunk)
+        if (!flag) {
+            // 下一次写入会有背压
+            rs.pause() // 先暂停读取，等待写
+        }
+    })
+    ws.on("drain", () => {
+        rs.resume() // 干涸了，可以继续读
+    })
+    rs.on("end", () => {
+        console.timeEnd("3. 文件流")
+    })
+
+    // 4. 文件流pipe() 内部自动处理了背压
+    const ws4 = fs.createWriteStream(to(4))
+    console.time("4. 文件流pipe")
+    rs.pipe(ws4)
+    rs.on("end", () => {
+        console.timeEnd("4. 文件流pipe")
+    })
+    /*
+        1. copyFile: 0.862ms
+        2. 先 read 再 write: 0.719ms
+        3. 文件流: 2.555ms
+        4. 文件流pipe: 2.302ms
+    */
+
+    for (let i = 1; i < 5; i++)
+        await fs.promises.unlink(to(i)) // 删除文件
 })()
