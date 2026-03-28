aa785fb8412b3e2c158f94c32630922ac5936750
WJH <3506456886@qq.com>
2026-03-28 14:35:37 +0800

    feat[ptr]: NodeJS API readdir读取目录状态 mkdir创建目录

---
+++ NodeJS/sandboxs/fsDir.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const path = require('path')
+
+const test = async () => {
+  const res = await fs.promises.readdir(__dirname)
+  console.log(res)
+
+  for (let i = 3; i <= 6; i++) {
+    // await 就阻塞了，不是并行了
+    const dir = path.resolve(__dirname, `./testFiles/${i}`)
+    const file = path.resolve(dir, 'index.txt')
+    fs.promises.mkdir(dir, { recursive: true }) // 参数对象：如果父目录不存在那就连同父目录一同创建
+      .then(() => fs.promises.writeFile(file, `ceilf${i}`))
+    // then 中传入的是后续的异步处理函数
+  }
+}
+
+test()
+/*
+[
+  'GlobalObject.js',   'fsReadFile.js',
+  'fsReaddir.js',      'fsStat.js',
+  'fsWriteFile.js',    'index.js',
+  'mainDec.js',        'node_modules',
+  'package-lock.json', 'package.json',
+  'testCMJ.cjs',       'testESMinCMJ.mjs',
+  'testFiles', // 可以读到目录文件，但是无法递归读取深度文件
+  'testImportCMJ.js',
+  'testJSON.json',     'testOS.js',
+  'testPath.js',       'testURL.js',
+  'testUtil.js'
+]
+*/
